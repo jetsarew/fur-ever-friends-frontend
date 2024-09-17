71514981e93982db6b4212b41db57a8ba1405041
Jetsarit Kaewsaart <applerew14112546@gmail.com>
2024-09-17 08:42:52 +0700

    feat: Improve ActivityStateBar to be able to display both pet sitter side and pet owner side

--- src/components/Card/ActivityStateBar.tsx
+++ src/components/Card/ActivityStateBar.tsx
@@ -1,11 +1,17 @@
+type activityState = "Unassigned" | "Assigned" | "In progress" | "Returning" | "Completed" | "Requested" | "Scheduled";
+
 export default function ActivityStateBar(){
-    const currentState = "Unassigned";
+    const currentState = "Requested";
+    const role: "pet sitter" | "pet owner" = "pet sitter";
+
     const statePriority = {
         "Unassigned": 0,
         "Assigned": 1,
         "In progress": 2,
         "Returning": 3,
         "Completed": 4,
+        "Requested": 0,
+        "Scheduled": 1,
     };
 
     const pastDotElement = <div className="w-3 h-3 rounded-full bg-dark-blue"></div>;
@@ -17,17 +23,25 @@ export default function ActivityStateBar(){
 
     const activeUnassignedElement = (
         <div className="h-[151px] pt-[2px] flex flex-col items-end gap-4">
-            <p className="text-subheading text-bright-blue">Unassigned</p>
-            <div className="flex flex-col items-end gap-1">
-                <p className="text-end text-body-paragraph">Please choose a pet sitter at least 24 hours before the activity begins</p> 
-                <button className="px-6 py-2 rounded-lg bg-bright-blue text-button text-white">See all request</button>
-            </div>
+            <p className="text-subheading text-bright-blue">{`${role == "pet sitter" ? "Requested" : "Unassigned"}`}</p>
+            {
+                role == "pet sitter" ? 
+                <div className="flex flex-col items-end gap-1">
+                    <p className="text-end text-body-paragraph">Waiting for pet owner approval</p> 
+                </div>
+                :
+                <div className="flex flex-col items-end gap-1">
+                    <p className="text-end text-body-paragraph">Please choose a pet sitter at least 24 hours before the activity begins</p> 
+                    <button className="px-6 py-2 rounded-lg bg-bright-blue text-button text-white">See all request</button>
+                </div>
+            }
+            
         </div>
     );
 
     const activeAssignedElement = (
         <div className="h-[151px] pt-[2px] flex flex-col items-end gap-4">
-            <p className="text-subheading text-bright-blue">Assigned</p>
+            <p className="text-subheading text-bright-blue">{`${role == "pet sitter" ? "Scheduled" : "Assigned"}`}</p>
             <div className="flex flex-row justify-end items-baseline gap-1 text-body">
                 <p>The activity starts in</p>
                 <p className="text-subheading">8</p>
@@ -54,33 +68,49 @@ export default function ActivityStateBar(){
     const activeReturningElement = (
         <div className="h-[151px] pt-[1px] flex flex-col items-end gap-4">
             <p className="text-subheading text-bright-blue">Returning</p>
-            <div className="flex flex-col items-end gap-1">
-                <p className="text-end text-body-paragraph">Have all your pets been returned to you yet?</p> 
-                <button className="px-6 py-2 rounded-lg bg-bright-green text-button text-white">I have received my pets</button>
-                <button className="h-8 px-6 py-2 flex flex-row justify-center items-center rounded-lg border-[2px] border-bright-red text-body-bold text-bright-red">Report an issue</button>
-            </div>
+            {
+                role == "pet sitter" ?
+                <div className="flex flex-col items-end gap-1">
+                    <p className="text-end text-body-paragraph">Have you returned all the pets to the owner yet?</p> 
+                    <button className="px-6 py-2 rounded-lg bg-bright-green text-button text-white">I have returned all pets</button>
+                </div>
+                :
+                <div className="flex flex-col items-end gap-1">
+                    <p className="text-end text-body-paragraph">Have all your pets been returned to you yet?</p> 
+                    <button className="px-6 py-2 rounded-lg bg-bright-green text-button text-white">I have received my pets</button>
+                    <button className="h-8 px-6 py-2 flex flex-row justify-center items-center rounded-lg border-[2px] border-bright-red text-body-bold text-bright-red">Report an issue</button>
+                </div>
+            }
         </div>
     );
 
     const activeCompletedElement = (
         <div className="h-[149px] pt-[2px] flex flex-col items-end gap-4">
             <p className="text-subheading text-bright-blue">Completed</p>
-            <div className="flex flex-col items-end gap-1">
-                <p className="text-end text-body-paragraph">Please review the pet sitter</p> 
-                <button className="px-6 py-2 rounded-lg bg-golden-yellow text-button text-white">Rate pet sitter</button>
-            </div>
+            {
+                role == "pet sitter" ?
+                <div className="flex flex-col items-end gap-1">
+                    <p className="text-end text-body-paragraph">You have successfully completed the activity</p> 
+                </div>
+                :
+                <div className="flex flex-col items-end gap-1">
+                    <p className="text-end text-body-paragraph">Please review the pet sitter</p> 
+                    <button className="px-6 py-2 rounded-lg bg-golden-yellow text-button text-white">Rate pet sitter</button>
+                </div>
+            }
+            
         </div>
     );
 
     const inactiveUnassignedElement = (
         <div className="h-[68px]">
-            <p className={`text-body ${statePriority[currentState] < statePriority["Unassigned"] ? "text-soft-gray" : "text-dark-blue"}`}>Unassigned</p>
+            <p className={`text-body ${statePriority[currentState] < statePriority["Unassigned"] ? "text-soft-gray" : "text-dark-blue"}`}>{`${role == "pet sitter" ? "Requested" : "Unassigned"}`}</p>
         </div>
     );
 
     const inactiveAssignedElement = (
         <div className="h-[68px]">
-            <p className={`text-body ${statePriority[currentState] < statePriority["Assigned"] ? "text-soft-gray" : "text-dark-blue"}`}>Assigned</p>
+            <p className={`text-body ${statePriority[currentState] < statePriority["Assigned"] ? "text-soft-gray" : "text-dark-blue"}`}>{`${role == "pet sitter" ? "Scheduled" : "Assigned"}`}</p>
         </div>
     );
 
@@ -102,17 +132,17 @@ export default function ActivityStateBar(){
         </div>
     );
 
-    const renderState = (checkedState: string, activeElement: JSX.Element, inactiveElement: JSX.Element) => {
-        return currentState === checkedState ? activeElement : inactiveElement;
+    const renderState = (checkedState: activityState, activeElement: JSX.Element, inactiveElement: JSX.Element) => {
+        return statePriority[currentState] === statePriority[checkedState] ? activeElement : inactiveElement;
     }
 
-    const renderDot = (checkedState: "Unassigned" | "Assigned" | "In progress" | "Returning" | "Completed") => {
+    const renderDot = (checkedState: activityState) => {
         if(statePriority[currentState] > statePriority[checkedState]) return pastDotElement;
         if(statePriority[currentState] == statePriority[checkedState]) return activeDotElement;
         return inactiveDotStyleElement;
     };
 
-    const renderLine = (checkedState: "Unassigned" | "Assigned" | "In progress" | "Returning" | "Completed") => {
+    const renderLine = (checkedState: activityState) => {
         if(statePriority[currentState] > statePriority[checkedState]) return pastLineElement;
         if(statePriority[currentState] == statePriority[checkedState]) return activeLineElement;
         return inactiveLineElement;
