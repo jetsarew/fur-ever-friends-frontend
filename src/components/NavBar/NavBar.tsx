7a226d71417a467c7e4acfdbbd0c54942ff0fc8f
Jetsarit Kaewsaart <applerew14112546@gmail.com>
2024-09-29 14:49:43 +0700

    feat: init NavBar for all role and Footer

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,73 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProfileImage from "./ProfileImage";
+import { BellIcon } from "@/shared/Icon";
+import Notification from "./Notification";
 
 export default function NavBar(){
+    let state = "users";
+    let role = "pet owner";
+
+    state = "users";
+    role = "pet owner";
+
     return (
         <div className="fixed top-0 left-0 right-0 w-full h-16 px-[111px] flex flex-row justify-between items-center border-b border-bd-gray bg-white">
             <Link href={"/"} className="text-header text-bright-blue">FUR-EVER FRIENDS</Link>
-            <div className="flex flex-row justify-between gap-[102px]">
-                <div className="flex flex-row items-center gap-6 color text-button text-medium-gray">
+            {
+                state == "users" ?
+                <div className="flex flex-row justify-between gap-[102px]">
+                    {
+                        role == "pet owner" &&
+                        <div className="flex flex-row items-center gap-6 color text-button text-medium-gray">
+                            <Link
+                                href="/activity"
+                            >Activities</Link>
+                            <Link
+                                href="/pet"
+                            >My pets</Link>
+                            <Notification />
+                        </div>
+                    }
+                    {
+                        role == "pet sitter" &&
+                        <div className="flex flex-row items-center gap-6 color text-button text-medium-gray">
+                            <Link
+                                href="/search"
+                            >Browse activities</Link>
+                            <Link
+                                href="/activity"
+                            >My tasks</Link>
+                            <BellIcon />
+                        </div>
+                    }
+                    {
+                        role == "admin" &&
+                        <div className="flex flex-row items-center gap-6 color text-button text-medium-gray">
+                            <Link
+                                href="/users"
+                            >Manage users</Link>
+                            <Link
+                                href="/reports"
+                            >View reports</Link>
+                            <Link
+                                href="/applications"
+                            >Pet sitter applications</Link>
+                            <BellIcon />
+                        </div>
+                    }
+                    <ProfileImage />
+                </div> :
+                <div className="flex flex-row justify-between items-center gap-[29px]">
                     <Link
-                        href="/activity"
-                    >Activities</Link>
+                        href="/"
+                        className="text-button text-bright-blue"
+                    >Log in</Link>
                     <Link
-                        href="/pet"
-                    >My pets</Link>
+                        href="/"
+                        className="px-6 py-4 flex flex-row justify-center items-center rounded-lg text-button text-white bg-bright-blue"
+                    >Join now</Link> 
                 </div>
-                <Image 
-                    src={"/profile.jpg"}
-                    width={144}
-                    height={144}
-                    alt={"profile image"}
-                    className="w-12 h-12 rounded-full object-cover"
-                />
-            </div>
+            }
         </div>
     )
 }
