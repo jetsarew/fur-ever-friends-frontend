51c8aa0a9bfadbc6bdd2901e99d58c066bd6194f
NTPNTPNTP <natapat.raungput.2547@gmail.com>
2024-10-05 09:15:26 +0700

    add filter(UI only) on users and reports page

--- src/components/Admin/Button/Filter.tsx
+++ src/components/Admin/Button/Filter.tsx
@@ -23,12 +23,12 @@ export function UsersFilter() {
     }
 
     return (
-        <div>
+        <div className='flex gap-[16px]'>
             <div onClick={handleState} className='w-[26px] cursor-pointer select-none'>
                 <FilterIcon />
             </div>
             {!isClosed &&
-                <div className='flex w-max gap-[20px] relative top-9'>
+                <div className='flex w-max gap-[20px]'>
                     <div className='flex w-[245px] gap-[8px] select-none'>
                         <div onClick={handleStatusChange}>
                             {isStatusChecked ? (
@@ -89,44 +89,56 @@ export function UsersFilter() {
 
 export function ReportsFilter() {
     const [isDateChecked, setIsDateChecked] = useState(false);
-
+    const [isClosed, setIsClosed] = useState(true);
 
     const handleDateChange = () => {
         setIsDateChecked(!isDateChecked);
     };
 
+    const handleState = () => {
+        setIsClosed(!isClosed);
+    }
+
     return (
-        <div className='flex w-[220px]'>
-            <div className='flex gap-[8px] select-none'>
-                <div onClick={handleDateChange}>
-                    {isDateChecked ? (
-                        <CheckboxIcon />
-                    ) : (
-                        <NullCheckboxIcon />
-                    )}
-                </div>
-                <div className='flex flex-col gap-[8px]'>
-                    <div className='text-body-bold text-dark'>
-                        Date
-                    </div>
-                    {isDateChecked &&
-                        <div className='flex flex-col w-[196px] gap-[8px]'>
-                            <div className='flex justify-between items-center'>
-                                <div className='text-body text-dark'>
-                                    from
-                                </div>
-                                <CalendarCard />
-                            </div>
-                            <div className='flex justify-between items-center'>
-                                <div className='text-body text-dark'>
-                                    to
-                                </div>
-                                <CalendarCard />
-                            </div>
-                        </div>
-                    }
-                </div>
+        <div className='flex gap-[16px]'>
+            <div onClick={handleState} className='w-[26px] cursor-pointer select-none'>
+                <FilterIcon />
             </div>
+            {!isClosed &&
+
+                <div className='flex w-[220px]'>
+                    <div className='flex gap-[8px] select-none'>
+                        <div onClick={handleDateChange}>
+                            {isDateChecked ? (
+                                <CheckboxIcon />
+                            ) : (
+                                <NullCheckboxIcon />
+                            )}
+                        </div>
+                        <div className='flex flex-col gap-[8px]'>
+                            <div className='text-body-bold text-dark'>
+                                Date
+                            </div>
+                            {isDateChecked &&
+                                <div className='flex flex-col w-[196px] gap-[8px]'>
+                                    <div className='flex justify-between items-center'>
+                                        <div className='text-body text-dark'>
+                                            from
+                                        </div>
+                                        <CalendarCard defaultDate={3} />
+                                    </div>
+                                    <div className='flex justify-between items-center'>
+                                        <div className='text-body text-dark'>
+                                            to
+                                        </div>
+                                        <CalendarCard defaultDate={5} />
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
--- src/components/Admin/Card/CalendarCard.tsx
+++ src/components/Admin/Card/CalendarCard.tsx
@@ -1,4 +1,6 @@
-export default function CalendarCard({ }) {
+export default function CalendarCard({ defaultDate }: {
+    defaultDate: number;
+}) {
     const getDateAfterDays = (days: number) => {
         const date = new Date();
         date.setDate(date.getDate() + days);
@@ -7,9 +9,7 @@ export default function CalendarCard({ }) {
 
     return (
         <div className="flex justify-center items-center h-[46px] w-[148px] rounded-[8px] border border-light-gray2 cursor-pointer select-none">
-            {/* <div className="text-body text-dark">{date}</div> */}
-            {/* <CalendarIcon /> */}
-            <input type="date" defaultValue={getDateAfterDays(3)} className="w-[120px] cursor-pointer" />
+            <input type="date" defaultValue={getDateAfterDays(defaultDate)} className="w-[120px] cursor-pointer" />
         </div>
     );
 }
